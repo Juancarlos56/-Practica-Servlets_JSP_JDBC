0ebdf82df718511c85a9c18851ea1f9f2f3992ad
Juancarlos56 <barrerajuan930@gmail.com>
2020-12-02 02:33:59 -0500

    Login, index, modelo, controlador, jdbc

---
+++ Practica-Servlets_JSP_JDBC/WebContent/js/buscarProducto.js
@@ -0,0 +1,60 @@
+function opcionBusqueda(valor) {
+    if (valor == "Cedula") {
+            document.getElementById("Formulario01").style.display = "inline";
+            document.getElementById("Formulario02").style.display = "none";
+    }
+
+    if (valor == "Correo") {
+        document.getElementById("Formulario01").style.display = "none";
+        document.getElementById("Formulario02").style.display = "inline";
+    }
+}
+
+function buscarPro(idEmp, nomPro) {
+	
+	if(nomPro != ""){
+		
+		
+	    if (window.XMLHttpRequest) {
+	        // code for IE7+, Firefox, Chrome, Opera, Safari
+	        xmlhttp = new XMLHttpRequest();
+	    } else {
+	        // code for IE6, IE5
+	        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+	    }
+	    xmlhttp.onreadystatechange = function() {
+	        if (this.readyState == 4 && this.status == 200) {
+	            //alert("llegue");
+	            document.getElementById("informacion").innerHTML = this.responseText;
+	        }
+	    };
+	    xmlhttp.open("GET", "/buscarProducto?nomPro=" + nomPro +"&idEmp="+idEmp, true);
+	    xmlhttp.send();
+	}
+	
+	return false;
+}
+
+function buscarPorCorreo() {
+    var correo = document.getElementById("correo").value;
+    if (correo == "") {
+        document.getElementById("informacion").innerHTML = "";
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                //alert("llegue");
+                document.getElementById("informacion").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET", "../../controladores/listarTelefonosCorreo.php?correo=" + correo, true);
+        xmlhttp.send();
+    }
+    return false;
+}
---
+++ Practica-Servlets_JSP_JDBC/WebContent/js/listaProductosPublicos.js
@@ -0,0 +1,14 @@
+function showMenu(elmnt) {
+    document.getElementById(elmnt).style.visibility = "visible";
+    console.log("imprimiendo el elemento")
+
+}
+
+//function hideMenu(elmnt) {
+//  document.getElementById(elmnt).style.visibility = "hidden";
+//console.log(elmnt)
+//}
+
+function listarProducto() {
+
+}
